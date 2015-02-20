956232557bc63ed16bef8032f788c51a8a822294
François Greze <estate.mailer@gmail.com>
2015-02-20 11:14:20 +0100

    folder structure

--- app/app.js
+++ app/app.js
@@ -1,23 +1,13 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  console.log(Meteor.settings);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+  console.log("Meteor settings :");
+  console.log(Meteor.settings);
+
+  console.log("");
+  console.log("Environment variable :");
+  console.log(process.env.MAIL_URL);
 }
